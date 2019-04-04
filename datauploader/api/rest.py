e7bc1e304b3a6bd6aa23166140107be3f6f7a451
carolinux <carolinegr@gmail.com>
2019-04-04 23:49:43 +0100

    Add method for getting user repos sorted by creation time DESC

--- datauploader/api/rest.py
+++ datauploader/api/rest.py
@@ -1,8 +1,18 @@
 import json
 import requests
 
+# sort order is most recently created first
+# max page size = 100 (may not be respected or vary, but this is the max max)
+# https://developer.github.com/v3/#pagination
+
 
 GITHUB_RATE_LIMIT_ENDPOINT = "https://api.github.com/rate_limit"
+GITHUB_REPOS_ENDPOINT = "https://api.github.com/user/repos?sort=created&per_page=100"
+
+
+def get_auth_header(github_access_token):
+    auth_header = {"Authorization": "Bearer " + github_access_token}
+    return auth_header
 
 
 def get_rate_limit_remaining(github_access_token, type="core"):
@@ -14,7 +24,24 @@ def get_rate_limit_remaining(github_access_token, type="core"):
 
     """
 
-    auth_header = {"Authorization": "Bearer " + github_access_token}
+    auth_header = get_auth_header(github_access_token)
     response = requests.get(GITHUB_RATE_LIMIT_ENDPOINT, headers=auth_header)
     rate_limit_info = json.loads(response.content)['resources'][type]
     return rate_limit_info['remaining'], rate_limit_info['reset']
+
+
+def get_user_repos(github_access_token):
+    results = []
+    cnt = 0
+    url = GITHUB_REPOS_ENDPOINT
+    while(True):
+        cnt+=1
+        response = requests.get(url, headers=get_auth_header(github_access_token))
+        results += json.loads(response.content)
+        next = response.links.get('next')
+        if not next:
+            break
+        else:
+            url = next['url']
+    #print("Called the api {} times".format(cnt))
+    return results
