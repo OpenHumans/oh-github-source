3a2abd650acf30b3c7242d6481d1586d31f12aad
carolinux <carolinegr@gmail.com>
2019-08-06 10:15:17 +0100

    Adding on top of existing data now works

--- datauploader/api/rest.py
+++ datauploader/api/rest.py
@@ -43,7 +43,7 @@ def get_latest_github_file_url(oh_access_token):
     download_url = None
     last_updated_at = None
     for dfile in member['data']:
-        if 'GoogleFit' in dfile['metadata']['tags']:
+        if 'Github' in dfile['metadata']['tags']:
             if last_updated_at is None or dfile['metadata'].get('updated_at', '') >= last_updated_at:
                 last_updated_at = dfile['metadata']['updated_at']
                 download_url = dfile['download_url']
@@ -114,7 +114,7 @@ def get_repo_commits_for_user(github_access_token, repo, username, sync_after_da
         if latest_commit_date is None and len(commits) > 0:
             # github returns the data in descending chronological order
             # date is in the format 2014-05-09T15:14:07Z
-            latest_commit_date = get_commit_date(commits)
+            latest_commit_date = get_commit_date(commits[0])
 
         if sync_after_date is not None:
             for idx, commit in enumerate(commits):
@@ -122,6 +122,8 @@ def get_repo_commits_for_user(github_access_token, repo, username, sync_after_da
                 # this may not handle rebases/history rewrites 100% correctly
                 # is good effort for gathering our commit histories <:o)
                 if commit_date <= sync_after_date:
+                    print("reached existing data at idx {}".format(idx))
+                    print("will only add {} to existing".format(commits[:idx]))
                     reached_previous_data = True
                     commits = commits[:idx]
                     break
@@ -149,8 +151,14 @@ class GithubData(object):
     def get_last_commit_date_for_repo(self, repo):
 
         if repo in self.repo_data:
-            return self.repo_data['last_commit_date']
+            return self.repo_data[repo]['last_commit_date']
         return None
+
+    def get_commits_for_repo(self, repo):
+
+        if repo in self.repo_data:
+            return self.repo_data[repo]['commits']
+        return []
 
 
     def __repr__(self):
@@ -161,23 +169,30 @@ class GithubData(object):
 
         print("Starting rate limit status:")
         print(get_rate_limit_remaining(token))
-        username = get_user_info(token).get('login')
+        user_info = get_user_info(token)
+        username = user_info.get('login')
         repos = get_user_repos(token)
         repo_names = get_full_names_from_repos(repos)
 
         repo_data = {}
         for repo_name in repo_names:
-            print("Fetching commits for {}".format(repo_name))
+            print("Fetching new commits for {}".format(repo_name))
 
-            last_existing_commit_date = existing_data.get_last_commit_date_for_repo(repo_name)
+            if existing_data:
+                last_existing_commit_date = existing_data.get_last_commit_date_for_repo(repo_name)
+            else:
+                last_existing_commit_date = None
 
             repo_commits, latest_date = get_repo_commits_for_user(token, repo_name, username, sync_after_date=last_existing_commit_date)
-            print("Fetched {} commits".format(len(repo_commits)))
-            # TODO: here, if this repo exists in the existing data, need to merge the commits
+            print("Fetched {} new commits".format(len(repo_commits)))
+
+            if existing_data:
+                repo_commits = repo_commits + existing_data.get_commits_for_repo(repo_name)
+
             repo_data[repo_name] = {"commits": repo_commits, "last_commit_date": latest_date,
                                     "num_commits": len(repo_commits)}
 
-        metadata = {"username": username, "num_repos": len(repos)}
+        metadata = {"user_info": user_info, "num_repos": len(repos)}
 
         print("Ending rate limit status:")
         print(get_rate_limit_remaining(token))
